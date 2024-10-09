dec6e5e21af0f1216b0490af99c11a1ba8355c2d
feherdorian <dorianfeherzsolt@gmail.com>
2024-10-09 09:27:48 +0200

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="home">
-    <h2 class="led-text">Üdvözöljük a Kosárlabdázók világában!</h2>
-    <p class="led-text">Itt megtalálhatod a legnagyobb kosárlabdázók adatait és statisztikáit.</p>
+    <h2 class="led-text">{{ titleText }}</h2>
+    <p class="led-text">{{ displayedText }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      title: "Üdvözöljük a Kosárlabdázók világában!",
+      fullText: "Itt megtalálhatod a legnagyobb kosárlabdázók adatait és statisztikáit.",
+      titleText: "",
+      displayedText: "",
+      titleIndex: 0,
+      textIndex: 0,
+    };
+  },
+  mounted() {
+    this.typeTitle();
+  },
+  methods: {
+    typeTitle() {
+      if (this.titleIndex < this.title.length) {
+        this.titleText += this.title.charAt(this.titleIndex);
+        this.titleIndex++;
+        setTimeout(this.typeTitle, 70); 
+      } else {
+       
+        this.typeText();
+      }
+    },
+    typeText() {
+      if (this.textIndex < this.fullText.length) {
+        this.displayedText += this.fullText.charAt(this.textIndex);
+        this.textIndex++;
+        setTimeout(this.typeText, 70); 
+      }
+    },
+  },
 };
 </script>
 
@@ -22,6 +54,7 @@ export default {
     0 0 5px rgba(163, 3, 17, 1), 
     0 0 10px rgba(163, 3, 17, 0.7); 
   margin: 10px 0; 
+  animation: glow 1.5s infinite alternate;
 }
 
 @keyframes glow {
